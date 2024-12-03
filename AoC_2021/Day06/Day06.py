e31390cd626e0b9cce1a156fd7d344d107c09865
olliklee <okleemann@gmail.com>
2024-12-03 18:35:56 +0100

    added and refactured day 07/2021

--- AoC_2021/Day06/Day06.py
+++ AoC_2021/Day06/Day06.py
@@ -4,25 +4,26 @@
 from aoc_helper import *
 import re
 
-year, day = "2021", "04"
+year, day = "2021", "06"
 
 
 def solve():
     numbers = list(map(int, load_input().split(',')))
+
     part_a = part_b = 0
-    haeufigkeiten = {key: numbers.count(key) for key in range(9)}
+    freq = {key: numbers.count(key) for key in range(9)}
 
     for d in range(257):
-        zero = haeufigkeiten[0]
-        haeufigkeiten[0] = 0
+        zero = freq[0]
+        freq[0] = 0
         for key in range(1, 9):
-            haeufigkeiten[key-1] += haeufigkeiten[key]
-            haeufigkeiten[key] = 0
+            freq[key-1] += freq[key]
+            freq[key] = 0
 
-        haeufigkeiten[6] += zero
-        haeufigkeiten[8] += zero
-        if d == 79: part_a = sum(haeufigkeiten.values())
-        if d == 255: part_b = sum(haeufigkeiten.values())
+        freq[6] += zero
+        freq[8] += zero
+        if d == 79: part_a = sum(freq.values())
+        if d == 255: part_b = sum(freq.values())
 
     return part_a, part_b
 
