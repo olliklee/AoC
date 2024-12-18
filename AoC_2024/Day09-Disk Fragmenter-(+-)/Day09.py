39571bc3a909de6a702f0fcb8251fac34bfb01f3
olliklee <okleemann@gmail.com>
2024-12-18 16:11:00 +0100

    day09 2 stars (part2 from Gravitar)

--- AoC_2024/Day09-Disk Fragmenter-(+-)/Day09.py
+++ AoC_2024/Day09-Disk Fragmenter-(+-)/Day09.py
@@ -33,10 +33,34 @@ def compress1(storage):
             fillers -= 1
     return new_storage
 
+## solution from Gravitar (https://github.com/Gravitar64/Advent-of-Code-2024/blob/master/day09.py)
+def compress2(puzzle):
+        files, spaces = dict(), []
+        pos = 0
+        for i, n in enumerate(puzzle):
+            if i % 2:
+                spaces.append((pos, n))
+            else:
+                files[i // 2] = (pos, n)
+            pos += n
+        
+        for f_id, (file_pos, file_size) in reversed(files.items()):
+            for i, (space_pos, space_size) in enumerate(spaces):
+                if space_pos >= file_pos: break
+                if file_size > space_size: continue
+                
+                files[f_id] = (space_pos, file_size)
+                new_space_size = space_size - file_size
+                
+                if new_space_size == 0:
+                    spaces.pop(i)
+                else:
+                    spaces[i] = (space_pos + file_size, new_space_size)
+                break
+        return sum(sum(f_id * n for n in range(p, p + l)) for f_id, (p, l) in files.items())
+
 
 def solve():
-    part2 = 0
-
     puzzle = list(map(int, list(load_input(test=False))))
 
     storage = defaultdict(tuple)
@@ -45,9 +69,10 @@ def solve():
         storage[file_id] = (puzzle[i], puzzle[i + 1])
         file_id += 1
     storage[file_id] = (puzzle[-1], 0)
-
+    
     part1 = sum([i * int(file_id) for i, file_id in enumerate(compress1(storage))])
-    part2 = 0
+    
+    part2 = compress2(puzzle)
 
     return part1, part2
 
