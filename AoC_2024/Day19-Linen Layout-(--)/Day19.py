4be02d54508e0461463b97a66b425aab5c29011f
Olli <102247940+olliklee@users.noreply.github.com>
2024-12-19 11:35:56 +0100

    day19 1 star

--- AoC_2024/Day19-Linen Layout-(--)/Day19.py
+++ AoC_2024/Day19-Linen Layout-(--)/Day19.py
@@ -3,16 +3,42 @@
 
 from aoc_helper import *
 
-year, day = "2024", "xx"
+year, day = '2024', '19'
+
+
+def is_valid(towel, patterns, max_size, memo=None):
+    print('.', end='')
+    if not towel:
+        return True
+    if memo is None:
+        memo = {}
+    if towel in memo:
+        return memo[towel]
+
+    for size in range(1, max_size + 1):
+        if size > len(towel):
+            break
+        combi = towel[:size]
+        if combi in patterns:
+            if is_valid(towel[size:], patterns, max_size, memo):
+                memo[towel] = True
+                return True
+
+    memo[towel] = False
+    return False
 
 
 def solve():
-  puzzle = load_input(test=True)
-  
-  part1 = 0
-  part2 = 0
-  
-  return part1, part2
+    part1 = part2 = 0
+
+    patterns, towels = load_input(test=False).split('\n\n')
+
+    stripes = patterns.split(', ')
+    max_size = max([len(s) for s in stripes])
+    towels = towels.split('\n')
+    for i, towel in enumerate(towels):
+        part1 += 1 if is_valid(towel, stripes, max_size) else 0
+    return part1, part2
 
 
 #  ----------   Start   ----------   #
