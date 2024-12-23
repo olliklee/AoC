6232b2fad6d3126bdbb70f2dfc07164086b9dbbc
olliklee <okleemann@gmail.com>
2024-12-23 15:44:27 +0100

    day02/2022 refactored

--- AoC_2022/Day02-Rock Paper Scissors-(++)/Day02.py
+++ AoC_2022/Day02-Rock Paper Scissors-(++)/Day02.py
@@ -1,42 +1,31 @@
 #  Solutions of Advent of Code
 #  Oliver Kleemann
 
-from AoC_2022.aoc_helper import run
+from aoc_helper import *
+import re
 
 year, day = "2022", "02"
-final = f"Day{day}_input.txt"
-test = f"Day{day}_input_.txt"
-filename = final
-
-def prepare_input(file_name):
-    with open(file_name) as f:
-        game = []
-        for line in f:
-            line = line.strip().split()
-            game.append((line[0], line[1]))
-    return game
 
 
-def solve_a():
-    game = prepare_input(f"Day{day}_input.txt")
+def solve():
+    puzzle = load_input()
+    game = re.findall(r'(\D) (\D)', puzzle)
+    
     elves_choice = "ABC"  # rock, paper, scissor
     my_choice = "XYZ"  # rock, paper, scissor
     total_score = 0
     for my_round in game:
         opponent = elves_choice.index(my_round[0])
         me = my_choice.index(my_round[1])
-
+        
         total_score += my_choice.index(my_round[1]) + 1  # score for chosen option
         if opponent == me:  # draw
             total_score += 3
         elif opponent == (me + 2) % 3:  # I win
             total_score += 6
-
-    return total_score
-
-
-def solve_b():
-    game = prepare_input(f"Day{day}_input.txt")
+    
+    part1 = total_score
+    
     elves_choice = "ABC"  # rock, paper, scissor
     my_option = "XYZ"  # lose, draw, win
     total_score = 0
@@ -44,10 +33,12 @@ def solve_b():
         win_score = my_option.index(my_round[1]) * 3
         my_choice_score = (elves_choice.index(my_round[0]) + (my_option.index(my_round[1]) + 2) % 3) % 3 + 1
         total_score += win_score + my_choice_score
+    
+    part2 = total_score
+    
+    return part1, part2
 
-    return total_score
 
+#  ----------   Start   ----------   #
 
-game = prepare_input(filename)
-
-run(day, year, solve_a, solve_b)
+run_puzzle(day, year, solve)
