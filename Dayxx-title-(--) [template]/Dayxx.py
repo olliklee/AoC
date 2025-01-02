1062ee2af7069b9dc128f77bdd55fd58195644df
Olli <102247940+olliklee@users.noreply.github.com>
2025-01-02 14:38:00 +0100

    change to template file

--- Dayxx-title-(--) [template]/Dayxx.py
+++ Dayxx-title-(--) [template]/Dayxx.py
@@ -3,14 +3,14 @@
 
 from aoc_helper import *
 
-year, day = "2024", "xx"
+year, day = "20xx", "xx"
 
 
 def solve():
+    part1 = part2 = 0
     puzzle = load_input(test=True)
 
-    part1 = 0
-    part2 = 0
+    part1 = part2 = 0
 
     return part1, part2
 
