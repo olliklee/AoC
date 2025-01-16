b969e07011625ec43b855d02bdebdf278327e5d1
Olli <102247940+olliklee@users.noreply.github.com>
2025-01-16 23:35:03 +0100

    day18/2016 faster

--- AoC_2016/Day18-Like a Rogue-(++)/Day18.py
+++ AoC_2016/Day18-Like a Rogue-(++)/Day18.py
@@ -7,31 +7,30 @@ year, day = "2016", "18"
 
 
 def calc_tile(line, pos):
-    trap = ['^^.', '.^^', '^..', '..^']
-    length = len(line)
-    if pos == 0:
-        pattern = '.' + line[:2]
-    elif pos == length-1:
-        pattern = line[pos-1:] + '.'
-    else:
-        pattern = line[pos-1:pos+2]
-
-    return '^' if pattern in trap else '.'
+    line = '.' + line + '.'
+    return '.' if line[pos] == line[pos+2] else '^'
 
 
-def create_map(puzzle, size):
-    room = [puzzle]
+def generate_next_line(line):
+    next_line = ''.join([calc_tile(line, i) for i in range(len(line))])
+    return next_line, next_line.count('.')
+
+
+def process(puzzle, size):
+    line, safe_spaces = puzzle, puzzle.count('.')
     for i in range(size - 1):
-        room.append(''.join([calc_tile(room[-1], i) for i in range(len(puzzle))]))
-    return room
+        new_line, safe = generate_next_line(line)
+        safe_spaces += safe
+        line = new_line
+    return safe_spaces
 
 
 def solve():
     puzzle = '^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^...^^..^^^..^.^^^^'
     size_pt1, size_pt2 = 40, 400000
 
-    part1 = sum(line.count('.') for line in create_map(puzzle, size_pt1))
-    part2 = sum(line.count('.') for line in create_map(puzzle, size_pt2))
+    part1 = process(puzzle, size_pt1)
+    part2 = process(puzzle, size_pt2)
 
     return part1, part2
 
