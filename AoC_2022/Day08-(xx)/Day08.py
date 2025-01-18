15745d5368c660579c861fea041eab193823237e
olliklee <okleemann@gmail.com>
2025-01-18 14:46:37 +0100

    Revert "change dict map function"
    This reverts commit 40d401fa50b3e54323170f7739fa32bc4b68bbe7.

--- AoC_2022/Day08-(xx)/Day08.py
+++ AoC_2022/Day08-(xx)/Day08.py
@@ -1,61 +1,41 @@
 # # #  Solutions of Advent of Code
 # # #  Oliver Kleemann
 
-from aoc_helper import *
+from time import perf_counter
 
 year, day = "2022", "08"
 
-'''
-3 0 3 7 3
-2 5 5 1 2
-6 5 3 3 2
-3 3 5 4 9
-3 5 3 9 0
-'''
+
+def run(d, y):
+    print(f"\nResults from AoC {y} - Day {d}\n{'-' * 30}")
+
+    start = perf_counter()
+    print(f"Day {d} - Part 1: {solve_a()}")
+
+    lap = perf_counter()
+    print(f"Day {d} - Part 2: {solve_b()}")
+
+    stop = perf_counter()
+
+    print(f"\nPerformance\n{'-' * 30}")
+    print(f"Part 1: {(lap - start) * 100:.6f} ms\nPart 2: {(stop - lap) * 100:.6f} ms")
+    print(f"{'-' * 30}\nGesamt: {(stop - start) * 100:.6f} ms")
 
 
-def is_visible(forest, pos, boundaries):
-    if not pos in forest:
-        return None
-    
-    x, y = pos
-    height = forest[pos]
-    max_x, max_y = boundaries
-    
-    if x == 0 or x == max_x or y == 0 or y == max_y:
-        return True
-    
-    row_before = [forest[x, y] for x in range(x)]
-    row_behind = [forest[x, y] for x in range(x + 1, max_x + 1)]
-    col_before = [forest[x, y] for y in range(y)]
-    col_behind = [forest[x, y] for y in range(y + 1, max_y + 1)]
-    
-    if any((max(row_before) < height, max(col_before) < height,
-           max(row_behind) < height, max(col_behind) < height)):
-        return True
-    else:
-        return False
-        
-        
-def solve():
-    part2 = 0
-    puzzle = load_input(test=False)
-    forest = text_to_dict_map(puzzle, int)
-    boundaries = next(reversed(forest.keys()))
-    visibles = set()
-    for pos in forest.keys():
-        if is_visible(forest, pos, boundaries):
-            visibles.add(pos)
-    part1 = len(visibles)
+def prepare_input(file_name):
+    with open(file_name) as f:
+        content = f.read()
+        program = content.split("\n")
 
-    return part1, part2
+    return program
 
 
 def solve_a():
-    forest = load_input(split_by_line=True)
-    dim_x, dim_y = len(forest[0]), len(forest)
+    forest = prepare_input(f"Day{day}_input.txt")
+    dim_x = len(forest[0])
+    dim_y = len(forest)
     visible = set()
-    
+
     # # Horizontal, L-R
     for i in range(dim_x):
         max_row = int(forest[i][0])
@@ -65,7 +45,7 @@ def solve_a():
             if tree > max_row:
                 max_row = tree
                 visible.add((i, j))
-    
+
     # # Horizontal, R-L
     for i in range(dim_y):
         max_row = int(forest[i][-1])
@@ -75,7 +55,7 @@ def solve_a():
             if tree > max_row:
                 max_row = tree
                 visible.add((i, j))
-    
+
     # Vertical, T-B
     for j in range(dim_y):
         maxtree = int(forest[0][j])
@@ -85,7 +65,7 @@ def solve_a():
             if tree > maxtree:
                 maxtree = tree
                 visible.add((i, j))
-    
+
     for j in range(dim_y):
         maxtree = int(forest[-1][j])
         visible.add((dim_x - 1, j))
@@ -94,16 +74,16 @@ def solve_a():
             if tree > maxtree:
                 maxtree = tree
                 visible.add((i, j))
-    
+
     return len(visible)
 
 
 def solve_b():
-    forest = load_input(split_by_line=True)
+    forest = prepare_input(f"Day{day}_input.txt")
     dim_x = len(forest[0])
     dim_y = len(forest)
     max_score = 0
-    
+
     def look_up(x, y):
         if y == 0:
             return 0
@@ -115,7 +95,7 @@ def solve_b():
                 break
             seen_trees += 1
         return seen_trees
-    
+
     def look_down(x, y):
         if y == dim_y - 1:
             return 0
@@ -127,7 +107,7 @@ def solve_b():
                 break
             seen_trees += 1
         return seen_trees
-    
+
     def look_right(x, y):
         if x == dim_x - 1:
             return 0
@@ -139,7 +119,7 @@ def solve_b():
                 break
             seen_trees += 1
         return seen_trees
-    
+
     def look_left(x, y):
         if x == 0:
             return 0
@@ -151,30 +131,13 @@ def solve_b():
                 break
             seen_trees += 1
         return seen_trees
-    
+
     for i in range(dim_x):
         for j in range(dim_y):
             score = look_up(i, j) * look_down(i, j) * look_right(i, j) * look_left(i, j)
             max_score = max(score, max_score)
-    
+
     return max_score
 
 
-#  ----------   Start   ----------   #
-
-run_puzzle(day,year, solve)
-run_puzzles(day,year,solve_a,solve_b)
-
-'''
-Results from AoC 2022 - Day 08
-------------------------------
-Day 08 - Part 1: 1700
-Day 08 - Part 2: 470596
-
-Performance
-------------------------------
-Part 1: 0.418767 ms
-Part 2: 2.014296 ms
-------------------------------
-Gesamt: 2.433063 ms
-'''
+run(day, year)
