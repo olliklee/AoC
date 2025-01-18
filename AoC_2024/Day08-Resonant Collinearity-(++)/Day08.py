40d401fa50b3e54323170f7739fa32bc4b68bbe7
olliklee <okleemann@gmail.com>
2025-01-18 14:45:36 +0100

    change dict map function

--- AoC_2024/Day08-Resonant Collinearity-(++)/Day08.py
+++ AoC_2024/Day08-Resonant Collinearity-(++)/Day08.py
@@ -32,7 +32,7 @@ def get_all_antipodes(point_a, point_b, grid):
 
 def solve():
   content = load_input(test=True)
-  puzzle = convert_to_dict_map(content)
+  puzzle = text_to_dict_map(content)
   
   antennas = defaultdict(list)
   for spot in puzzle:
