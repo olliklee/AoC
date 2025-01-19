1955d33a79bb18ab6afda6e1b11629fcdb7698ff
olliklee <okleemann@gmail.com>
2025-01-19 21:38:21 +0100

    day09/2022 changed and refactored to gravitar solution

--- AoC_2022/Day09-(x)/Day09.py
+++ AoC_2022/Day09-(x)/Day09.py
@@ -1,182 +1,49 @@
 # # #  Solutions of Advent of Code
 # # #  Oliver Kleemann
+# # #  inspired by gravitar
 
-from time import perf_counter
+from aoc_helper import *
+from pygame import Vector2 as V2
 
-year, day = "2022", "09"
-
-
-def run(d, y):
-    print(f"\nResults from AoC {y} - Day {d}\n{'-' * 30}")
-
-    start = perf_counter()
-    print(f"Day {d} - Part 1: {solve_a()}")
-
-    lap = perf_counter()
-    print(f"Day {d} - Part 2: {solve_b()}")
-
-    stop = perf_counter()
-
-    print(f"\nPerformance\n{'-' * 30}")
-    print(f"Part 1: {(lap - start) * 100:.6f} ms\nPart 2: {(stop - lap) * 100:.6f} ms")
-    print(f"{'-' * 30}\nGesamt: {(stop - start) * 100:.6f} ms")
+year, day = '2022', '09'
 
 
-def prepare_input(file_name):
-    with open(file_name) as f:
-        content = f.read()
-        way = content.split("\n")
-
-    return way
+def read_puzzle(file):
+    with open(file) as f:
+        return [line.strip().split() for line in f.readlines()]
 
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"({self.x},{self.y})"
-
-    def __repr__(self):
-        return f"({self.x},{self.y})"
-
-    def move(self, direction):
-        m = {"U": (0, -1), "D": (0, 1), "L": (-1, 0), "R": (1, 0)}
-        self.x += m[direction][0]
-        self.y += m[direction][1]
+def move(rope):
+    for i in range(len(rope) - 1):
+        s1, s2 = rope[i], rope[i + 1]
+        if s1.distance_to(s2) >= 2:
+            dx, dy = s1 - s2
+            if abs(dx) > 1:  dx //= abs(dx)
+            if abs(dy) > 1:  dy //= abs(dy)
+            rope[i + 1] = s2 + V2(dx, dy)
+    return rope
 
 
-class LongRope:
-    def __init__(self, points: [Point]):
-        self.points = points
-        self.lastmoved = False
-
-    def __str__(self):
-        output = ""
-        for p in self.points:
-            output += f"({p.x},{p.y})->"
-        return output.strip("->")
-
-    def __repr__(self):
-        output = ""
-        for p in self.points:
-            output += f"({p.x},{p.y})->"
-        return output.strip("->")
-
-    def move(self, num, direction):
-        if num >= len(self.points):
-            return "Done"
-
-        head = self.points[num]
-        tail = self.points[num + 1]
-        head.move(direction)
-
-        if direction in ['U', 'D']:
-            if abs(tail.y - head.y) > 1:
-                tail.x = head.x
-                if abs(tail.y - head.y) > 1:
-                    tail.move(direction)
-                    self.lastmoved = True
-        else:
-            if abs(tail.x - head.x) > 1:
-                tail.y = head.y
-                if abs(tail.x - head.x) > 1:
-                    tail.move(direction)
-                    self.lastmoved = True
-
-        self.points[num], self.points[num + 1] = head, tail
-
-    def fullmove(self, direction):
-        for i in range(len(self.points)-1):
-            self.move(i, direction)
-            if not self.lastmoved:
-                break
-        self.lastmoved = True
-
-    def printgraphic(self):
-        print(self.points)
-        for y in range(-5,5):
-            for x in range(-5,5):
-                if Point(x,y) in self.points:
-                    print("#", end="")
-                else:
-                    print(".", end="")
-            print()
-    # def move(self, direction, times=1):
-    #     for i in range(times):
-    #         self.move(direction)
-    #         self.follow_tail(direction)
-    #         tail_points_a.add((self.tail.x, self.tail.y))
+def move_all(puzzle, rope_length):
+    dir_dict = dict(R=V2(1, 0), L=V2(-1, 0), U=V2(0, -1), D=V2(0, 1))
+    
+    rope, tail_trail = [V2(0, 0)] * rope_length, set()
+    for direction, steps in puzzle:
+        for _ in range(int(steps)):
+            rope[0] = rope[0] + dir_dict[direction]
+            rope = move(rope)
+            tail_trail.add(tuple(rope[-1]))
+    return len(tail_trail)
 
 
-
-class Rope:
-    def __init__(self, head: Point, tail: Point):
-        self.head = head
-        self.tail = tail
-
-    def __str__(self):
-        return f"H{self.head}->{self.tail}"
-
-    def __repr__(self):
-        return f"H{self.head}->{self.tail}"
-
-    def move_head(self, direction, times=1):
-        for i in range(times):
-            self.head.move(direction)
-            self.follow_tail(direction)
-            tail_points_a.add((self.tail.x, self.tail.y))
-
-    def follow_tail(self, direction):
-        if direction in ['U', 'D']:
-            if abs(self.tail.y - self.head.y) > 2:
-                self.tail.x = self.head.x
-            elif (abs(self.tail.y - self.head.y)) > 1:
-                    self.tail.move(direction)
-        else:
-            if abs(self.tail.x - self.head.x) > 2:
-                self.tail.y = self.head.y
-            elif (abs(self.tail.x - self.head.x)) >1:
-                    self.tail.move(direction)
+def solve():
+    puzzle = [line.split() for line in load_input(split_by_line=True)]
+    part1 = move_all(puzzle, 2)
+    part2 = move_all(puzzle, 10)
+    
+    return part1, part2
 
 
-tail_points_a = set()
-tail_points_b = set()
+#  ----------   Start   ----------   #
 
-
-def solve_a():
-    way = prepare_input(f"Day{day}_input.txt")
-    head, tail = Point(0, 0), Point(0, 0)
-    rope = Rope(head, tail)
-
-    for m in way:
-        d, t = m.split()
-        rope.move_head(d, int(t))
-
-    return len(tail_points_a)
-
-
-def solve_b():
-    way = prepare_input(f"Day{day}_input_.txt")
-    rope: [Rope] = []
-    for i in range(6):
-        rope.append(Rope(Point(0,0),Point(0,0)))
-
-    for m in ["R", "R","R","R"]:
-        t = 1
-        for times in range(int(t)):
-            for i in range(len(rope)-1):
-                rope[i].move_head(m)
-                rope[i+1].head = rope[i].tail
-
-        for i in range(6):
-            print(rope[i])
-
-
-
-
-    return len(tail_points_b)
-
-
-run(day, year)
+run_puzzle(day, year, solve)
