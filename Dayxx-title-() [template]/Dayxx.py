6f90fb3e64e83c9cb4adbb82830be0e89b8b0f13
Olli <102247940+olliklee@users.noreply.github.com>
2025-12-06 23:45:02 +0100

    new template Dayxx file

---
+++ Dayxx-title-() [template]/Dayxx.py
@@ -0,0 +1,18 @@
+# # #  Solutions of Advent of Code
+# # #  Oliver Kleemann
+
+from aoc_helper import *
+
+year, day = "20xx", "xx"
+
+
+def solve():
+    part1 = part2 = 0
+    puzzle = load_input(test=True)
+
+    return part1, part2
+
+
+#  ----------   Start   ----------   #
+
+run_puzzle(day, year, solve)
